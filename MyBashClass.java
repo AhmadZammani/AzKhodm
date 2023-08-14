c1288b6532ffad1f8f9eadc650f93b60e123bfdf
Ahmad Farhad <farhad.zammani@gmail.com>
2023-08-14 07:26:27 -0700

    Adding new chnages

--- MyBashClass.java
+++ MyBashClass.java
@@ -3,7 +3,9 @@ package BashTest;
 public class MyBashClass {
 
 	public static void main(String[] args) {
-		System.out.println("This is a bash test");
+		System.out.println("This is a bash test and ");
+		System.out.println(" push this chnaged to the same repository");
+		
 
 	}
 
